c11f9142983c7eb94f8c5abe281a26d9c5754972
joaovillar <jaovillar@gmail.com>
2015-07-04 15:50:18 -0300

    Fix getTurmas Aluno e Professor

--- src/DAO/AulaDAO.java
+++ src/DAO/AulaDAO.java
@@ -26,8 +26,8 @@ public class AulaDAO {
 	private static final String DB_VERIFICA_CHAMADA_ABERTA = "select * from chamada where turma = ? and fim_aula = false";
 	private static final String DB_INICIALIZA_CHAMADA = "insert into chamada (turma, data_chamada, hora_inicio, fim_aula) values (?,?,?,false)";
 	private static final String DB_FINALIZA_CHAMADA = "update chamada set fim_aula = true where turma=?";
-	private static final String DB_GET_TURMA_PROFESSOR = "SELECT t.id, t.disciplina, d.nome, c.fim_aula FROM turma as t, disciplina as d, chamada as c WHERE datafim > CURRENT_TIMESTAMP and professor = (select id from usuario where usuario = ?) and t.disciplina = d.id and c.turma = t.id";
-	private static final String DB_GET_TURMA_ALUNO = "SELECT t.id, t.disciplina, d.nome, c.fim_aula FROM turma as t, disciplina as d, turma_aluno as ta, chamada as c WHERE t.datafim > CURRENT_TIMESTAMP and ta.aluno = (select id from usuario where usuario = ?) and t.disciplina = d.id and ta.turma = t.id and c.turma = t.id";
+	private static final String DB_GET_TURMA_PROFESSOR = "SELECT t.id, t.disciplina, d.nome FROM turma as t, disciplina as d WHERE datafim > CURRENT_TIMESTAMP and professor = (select id from usuario where usuario = ?) and t.disciplina = d.id";
+	private static final String DB_GET_TURMA_ALUNO = "SELECT t.id, t.disciplina, d.nome FROM turma as t, disciplina as d, turma_aluno as ta WHERE t.datafim > CURRENT_TIMESTAMP and ta.aluno = (select id from usuario where usuario = ?) and t.disciplina = d.id and ta.turma = t.id";
 	private static final String DB_ALUNO_EM_AULA = "insert into chamada_aluno(aluno_id, chamada_id, in_aula) values ((select id from usuario where usuario = ?),?,true)";
 	private static final String DB_VERIFICA_ALUNO_EM_AULA = "select * from chamada_aluno where aluno_id = (select id from usuario where usuario = ?) and in_aula = true";
 	private static final String DB_SAIR_AULA = "update chamada_aluno set in_aula = false where aluno_id = (select id from usuario where usuario = ?)";
@@ -105,11 +105,21 @@ public class AulaDAO {
 				while (rs.next()) {
 					turma = new Turma();
 					disciplina = new Disciplina();
-
+					
 					turma.setId(rs.getInt("id"));
-					turma.setChamadaAberta(rs.getBoolean("fim_aula"));
+					turma.setChamadaAberta(false);
 					disciplina.setId(rs.getInt("disciplina"));
 					disciplina.setNome(rs.getString("nome"));
+					
+					ps = connection.prepareStatement(DB_VERIFICA_CHAMADA_ABERTA);
+					
+					ps.setInt(1, rs.getInt("id"));
+					
+					ResultSet rs2 = ps.executeQuery();
+					
+					if(rs2.next()){
+						turma.setChamadaAberta(true);
+					}
 
 					turma.setDisciplina(disciplina);
 
@@ -150,11 +160,21 @@ public class AulaDAO {
 				while (rs.next()) {
 					turma = new Turma();
 					disciplina = new Disciplina();
-
+					
 					turma.setId(rs.getInt("id"));
-					turma.setChamadaAberta(rs.getBoolean("fim_aula"));
+					turma.setChamadaAberta(false);
 					disciplina.setId(rs.getInt("disciplina"));
 					disciplina.setNome(rs.getString("nome"));
+					
+					ps = connection.prepareStatement(DB_VERIFICA_CHAMADA_ABERTA);
+					
+					ps.setInt(1, rs.getInt("id"));
+					
+					ResultSet rs2 = ps.executeQuery();
+					
+					if(rs2.next()){
+						turma.setChamadaAberta(true);
+					}
 
 					turma.setDisciplina(disciplina);
 
